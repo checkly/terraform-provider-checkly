f08cf8bb5f63610ba5e5346f63e7f64cf31dde08
John Arundel <john@bitfieldconsulting.com>
2020-04-23 12:05:27 +0100

    Modify integration test to reproduce Issue #8

--- test.tf
+++ test.tf
@@ -78,42 +78,22 @@ resource "checkly_check" "test-check1" {
 }
 
 resource "checkly_check" "test-check2" {
-  name                   = "My test check 2"
-  type                   = "API"
-  activated              = true
-  should_fail            = true
-  frequency              = 1
-  ssl_check_domain       = "example.com"
-  double_check           = true
-  degraded_response_time = 15000
-  max_response_time      = 30000
+  name                      = "My test check 2"
+  type                      = "API"
+  activated                 = true
+  should_fail               = true
+  frequency                 = 1
+  ssl_check_domain          = "example.com"
+  double_check              = true
+  degraded_response_time    = 15000
+  max_response_time         = 30000
+  use_global_alert_settings = true
 
   locations = [
     "us-west-1",
     "ap-northeast-1",
     "ap-south-1",
   ]
-
-  alert_settings {
-    escalation_type = "RUN_BASED"
-
-    run_based_escalation {
-      failed_run_threshold = 1
-    }
-
-    time_based_escalation {
-      minutes_failing_threshold = 5
-    }
-
-    ssl_certificates {
-      enabled         = true
-      alert_threshold = 30
-    }
-
-    reminders {
-      amount = 1
-    }
-  }
 
   request {
     follow_redirects = true
