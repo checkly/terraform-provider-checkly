0191d8894e83c3e93ab89483898ffc97ad8bcb4b
John Arundel <john@bitfieldconsulting.com>
2020-05-19 14:58:30 +0100

    Update installer to pull releases from Checkly GH account

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ fi
 
 jq_cmd=".assets[] | select(.name | endswith(\"${platform}.gz\")).browser_download_url"
 # Find latest binary release URL for this platform
-url="$(curl -s https://api.github.com/repos/bitfield/terraform-provider-checkly/releases/latest | jq -r "${jq_cmd}")"
+url="$(curl -s https://api.github.com/repos/checkly/terraform-provider-checkly/releases/latest | jq -r "${jq_cmd}")"
 # Download the tarball
 curl -OL ${url}
 # Rename and copy to your Terraform plugin folder
