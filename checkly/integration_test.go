707d9fa750fa2c3037a127f435dfce1cbcf77f10
Khal Jouda <1508183+kjda@users.noreply.github.com>
2021-03-17 14:37:53 +0100

    fixing tests

---
+++ checkly/integration_test.go
@@ -0,0 +1,39 @@
+// +build integration
+
+package checkly
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func getAPIKey(t *testing.T) string {
+	key := os.Getenv("CHECKLY_API_KEY")
+	if key == "" {
+		t.Fatal("'CHECKLY_API_KEY' must be set for integration tests")
+	}
+	return key
+}
+
+func TestChecklyTerraformIntegration(t *testing.T) {
+	t.Parallel()
+	terraformOptions := &terraform.Options{
+		TerraformDir: ".",
+		Vars: map[string]interface{}{
+			"checkly_api_key": getAPIKey(t),
+		},
+	}
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+	planPath := "./test.plan"
+	exit, err := terraform.GetExitCodeForTerraformCommandE(t, terraformOptions, terraform.FormatArgs(terraformOptions, "plan", "--out="+planPath, "-input=false", "-lock=true", "-detailed-exitcode")...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(planPath)
+	if exit != terraform.DefaultSuccessExitCode {
+		t.Fatalf("want DefaultSuccessExitCode, got %d", exit)
+	}
+}
