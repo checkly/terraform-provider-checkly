d9401282725ad0012e65b54be4bb9a59bf7faeff
Pilar Checkly <pilarcheckly@192.168.1.3>
2021-11-09 16:07:50 -0300

    fix: add test data for dashboard

--- test.tf
+++ test.tf
@@ -538,13 +538,28 @@ resource "checkly_check_group" "group-with-alert-channels" {
   }
 }
 
-  resource "checkly_maintenance_windows" "maintenance-1" {
-  name = "string"
-  starts_at = "2014-08-24T00:00:00.000Z"
-  ends_at = "2014-08-25T00:00:00.000Z"
-  repeat_unit = "MONTH"
-  repeat_ends_at = "2014-08-24T00:00:00.000Z"
+resource "checkly_maintenance_windows" "maintenance-1" {
+  name            = "string"
+  starts_at       = "2014-08-24T00:00:00.000Z"
+  ends_at         = "2014-08-25T00:00:00.000Z"
+  repeat_unit     = "MONTH"
+  repeat_ends_at  = "2014-08-24T00:00:00.000Z"
   repeat_interval = 1
+  tags = [
+    "string",
+  ]
+}
+
+resource "checkly_dashboard" "dashboard-1" {
+  custom_url = "string"
+  custom_domain = "string"
+  logo= "logo"
+  header= "header"
+  refresh_rate= 60
+  paginate= false
+  pagination_rate= 30
+  hide_tags= false
+  width= "FULL"
   tags= [
     "string",
   ]
