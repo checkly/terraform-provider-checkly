c0a2ec7a5fea8055916eb0af91e1be314263de58
Nacho Anaya <nacho@checklyhq.com>
2022-01-06 13:39:28 -0300

    feat: add url property and clean up trigger resources (#106)
    * feat: add url property and clean up trigger resources
    
    * chore: update sdk version and docs
    
    * chore: update go.sum

--- checkly/resource_trigger_group.go
+++ checkly/resource_trigger_group.go
@@ -28,38 +28,42 @@ func resourceTriggerGroup() *schema.Resource {
 			"token": {
 				Type:     schema.TypeString,
 				Optional: true,
+				Computed: true,
+			},
+			"url": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
 			},
 		},
 	}
 }
 
-func triggerGroupFromResourceData(d *schema.ResourceData) (checkly.TriggerGroup, error) {
-	ID, err := strconv.ParseInt(d.Id(), 10, 64)
+func triggerGroupFromResourceData(data *schema.ResourceData) (checkly.TriggerGroup, error) {
+	ID, err := strconv.ParseInt(data.Id(), 10, 64)
 	if err != nil {
-		if d.Id() != "" {
+		if data.Id() != "" {
 			return checkly.TriggerGroup{}, err
 		}
 		ID = 0
 	}
-	a := checkly.TriggerGroup{
+
+	return checkly.TriggerGroup{
 		ID:      ID,
-		GroupId: int64(d.Get("group_id").(int)),
-		Token:   d.Get("token").(string),
-	}
-
-	fmt.Printf("%v", a)
-
-	return a, nil
+		GroupId: int64(data.Get("group_id").(int)),
+		Token:   data.Get("token").(string),
+	}, nil
 }
 
-func resourceDataFromTriggerGroup(s *checkly.TriggerGroup, d *schema.ResourceData) error {
-	d.Set("group_id", s.GroupId)
-	d.Set("token", s.Token)
+func resourceDataFromTriggerGroup(trigger *checkly.TriggerGroup, data *schema.ResourceData) error {
+	data.Set("group_id", trigger.GroupId)
+	data.Set("token", trigger.Token)
+	data.Set("url", trigger.URL)
 	return nil
 }
 
-func resourceTriggerGroupCreate(d *schema.ResourceData, client interface{}) error {
-	tc, err := triggerGroupFromResourceData(d)
+func resourceTriggerGroupCreate(data *schema.ResourceData, client interface{}) error {
+	tc, err := triggerGroupFromResourceData(data)
 	if err != nil {
 		return fmt.Errorf("resourceTriggerGroupCreate: translation error: %w", err)
 	}
@@ -71,58 +75,46 @@ func resourceTriggerGroupCreate(d *schema.ResourceData, client interface{}) erro
 		return fmt.Errorf("CreateTriggerGroup: API error: %w", err)
 	}
 
-	d.SetId(fmt.Sprintf("%d", result.ID))
-	d.Set("token", result.Token)
+	data.SetId(fmt.Sprintf("%d", result.ID))
 
-	return resourceTriggerGroupRead(d, client)
+	return resourceTriggerGroupRead(data, client)
 }
 
-func resourceTriggerGroupDelete(d *schema.ResourceData, client interface{}) error {
-	tc, err := triggerGroupFromResourceData(d)
+func resourceTriggerGroupDelete(data *schema.ResourceData, client interface{}) error {
+	tc, err := triggerGroupFromResourceData(data)
 	if err != nil {
-		return fmt.Errorf("resourceTriggerGroupCreate: translation error: %w", err)
+		return fmt.Errorf("resourceTriggerGroupDelete: translation error: %w", err)
 	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), apiCallTimeout())
 	defer cancel()
 	err = client.(checkly.Client).DeleteTriggerGroup(ctx, tc.GroupId)
 	if err != nil {
-		return fmt.Errorf("resourceTriggerGroupDelete: API error: %w", err)
+		return fmt.Errorf("DeleteTriggerGroup: API error: %w", err)
 	}
+
 	return nil
 }
 
-func resourceTriggerGroupRead(d *schema.ResourceData, client interface{}) error {
-	tc, err := triggerGroupFromResourceData(d)
+func resourceTriggerGroupRead(data *schema.ResourceData, client interface{}) error {
+	trigger, err := triggerGroupFromResourceData(data)
 	if err != nil {
-		return fmt.Errorf("resourceTriggerCheckDelete: ID %s is not numeric: %w", d.Id(), err)
+		return fmt.Errorf("resourceTriggerGroupRead: ID %s is not numeric: %w", data.Id(), err)
 	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), apiCallTimeout())
-	mw, err := client.(checkly.Client).GetTriggerGroup(ctx, tc.GroupId)
+	result, err := client.(checkly.Client).GetTriggerGroup(ctx, trigger.GroupId)
 	defer cancel()
 	if err != nil {
 		if strings.Contains(err.Error(), "404") {
-			d.SetId("")
+			data.SetId("")
 			return nil
 		}
-		return fmt.Errorf("resourceTriggerGroupRead: API error: %w", err)
+		return fmt.Errorf("GetTriggerGroup: API error: %w", err)
 	}
-	return resourceDataFromTriggerGroup(mw, d)
+	return resourceDataFromTriggerGroup(result, data)
 }
 
-func resourceTriggerGroupUpdate(d *schema.ResourceData, client interface{}) error {
-	tc, err := triggerGroupFromResourceData(d)
-	if err != nil {
-		return fmt.Errorf("resourceTriggerGroupRead: translation error: %w", err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), apiCallTimeout())
-	result, err := client.(checkly.Client).GetTriggerGroup(ctx, tc.GroupId)
-	defer cancel()
-
-	if err != nil {
-		return fmt.Errorf("resourceTriggerGroupUpdate: API error: %w", err)
-	}
-
-	d.Set("token", result.Token)
-
-	return resourceTriggerCheckRead(d, client)
+func resourceTriggerGroupUpdate(data *schema.ResourceData, client interface{}) error {
+	return resourceTriggerCheckRead(data, client)
 }
--- docs/resources/trigger_check.md
+++ docs/resources/trigger_check.md
@@ -7,7 +7,11 @@ Trigger check example
 
 ```terraform
 resource "checkly_trigger_check" "test-trigger-check" {
-   check_id      = "215"
+   check_id = "c1ff95c5-d7f6-4a90-9ce2-1e605f117592"
+}
+
+output "test-trigger-check-url" {
+  value = checkly_trigger_check.test-trigger-check.url
 }
 ```
 
