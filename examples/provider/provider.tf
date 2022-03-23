7249ef63577f19f8ccab7a8b55f3e8a40f3ae8c5
Ignacio Anaya <nacho@checklyhq.com>
2022-03-23 11:37:02 -0300

    feat: improve index docs page

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,23 +1,45 @@
-# Terraform 0.13+ uses the Terraform Registry:
+# Define a variable to hold the API Key and Account ID
+variable "checkly_api_key" {}
+variable "checkly_account_id" {}
 
+# Specify the Checkly provider
 terraform {
-  required_version = "= 1.1.17"
   required_providers {
     checkly = {
-      source  = "checkly/checkly"
+      source = "checkly/checkly"
       version = "1.4.3"
     }
   }
 }
 
-# Configure the Checkly provider
-variable "checkly_api_key" {
-}
-
-variable "checkly_account_id" {
-}
-
+# Pass the API Key environment variable to the provider
 provider "checkly" {
   api_key = var.checkly_api_key
   account_id = var.checkly_account_id
+}
+
+# Create your first API Check
+resource "checkly_check" "example-check" {
+  name                      = "Example API check"
+  type                      = "API"
+  activated                 = true
+  should_fail               = false
+  frequency                 = 10
+  double_check              = true
+  ssl_check                 = true
+  use_global_alert_settings = true
+
+  locations = [
+    "us-west-1"
+  ]
+
+  request {
+    url              = "https://api.example.com/"
+    follow_redirects = true
+    assertion {
+      source     = "STATUS_CODE"
+      comparison = "EQUALS"
+      target     = "200"
+    }
+  }
 }
