0eb138b65949598cc89269b862d3ba20597f0494
Ignacio Anaya <nacho@checklyhq.com>
2022-03-23 23:18:37 -0300

    feat: setup debug flag

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/checkly/terraform-provider-checkly/checkly"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug:        debugMode,
+		ProviderAddr: "registry.terraform.io/checkly/checkly",
 		ProviderFunc: checkly.Provider,
-	})
+	}
+
+	plugin.Serve(opts)
 }
