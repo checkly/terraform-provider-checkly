9bb88b3460697b9934e4d5a507e5f923a8ab6dbb
Ignacio Anaya <nacho@checklyhq.com>
2022-03-26 00:28:53 -0300

    refactor: split demo in different resource files

---
+++ demo/alert-channels.tf
@@ -0,0 +1,67 @@
+# Email Alert Channel
+resource "checkly_alert_channel" "email_ac" {
+  email {
+    address = "info@example.com"
+  }
+  send_recovery = true
+  send_failure  = false
+  send_degraded = true
+  ssl_expiry    = false
+}
+
+# Slack Alert Channel
+resource "checkly_alert_channel" "slack_ac" {
+  slack {
+    channel = "checkly_alerts"
+    url     = "https://slack.com/webhookurl"
+  }
+  send_recovery        = true
+  send_failure         = true
+  send_degraded        = false
+  ssl_expiry           = true
+  ssl_expiry_threshold = 11
+}
+
+# SMS Alert Channel
+resource "checkly_alert_channel" "sms_ac" {
+  sms {
+    name   = "smsalerts"
+    number = "4917512345678"
+  }
+}
+
+# Opsgenie Alert Channel
+resource "checkly_alert_channel" "opsgenie_ac" {
+  opsgenie {
+    name     = "opsalert"
+    api_key  = "key1"
+    region   = "EU"
+    priority = "P1"
+  }
+}
+
+# Pagerduty Alert Channel
+resource "checkly_alert_channel" "pagerduty_ac" {
+  pagerduty {
+    account      = "checkly"
+    service_key  = "key1"
+    service_name = "pdalert"
+  }
+}
+
+# Webhook Alert Channel
+resource "checkly_alert_channel" "webhook_ac" {
+  webhook {
+    name   = "webhhookalerts"
+    method = "get"
+    headers = {
+      X-HEADER-1 = "foo"
+    }
+    query_parameters = {
+      query1 = "bar"
+    }
+    template       = "tmpl"
+    url            = "https://example.com/webhook"
+    webhook_secret = "foo-secret"
+  }
+}
