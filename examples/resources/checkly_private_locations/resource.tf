fef6a01dfd81523122ec47482c6eeae7c24ba77f
Pilar Checkly <pilar@checklyhq.com>
2022-06-01 18:47:49 -0300

    fix: add correct example slug name

--- examples/resources/checkly_private_locations/resource.tf
+++ examples/resources/checkly_private_locations/resource.tf
@@ -1,5 +1,5 @@
 resource "checkly_private_locations" "location" {
   name          = "New Private Location"
-  slug_name     = "new_private_location"
+  slug_name     = "new-private-location"
   icon          = "location"
 }
