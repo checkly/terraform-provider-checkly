8ec33d2dd6d0414e137e146df4537279aa514de2
Pilar <47372639+pilimartinez@users.noreply.github.com>
2022-06-21 18:59:49 -0300

    feat: add support for private locations (#160)

--- demo/maintenance_windows.tf
+++ demo/maintenance_windows.tf
@@ -1,8 +1,8 @@
 # Simple Maintenance windows example
 resource "checkly_maintenance_windows" "maintenance-1" {
-  name            = "MW Simple"
-  starts_at       = "2028-08-24T00:00:00.000Z"
-  ends_at         = "2028-08-25T00:00:00.000Z"
+  name      = "MW Simple"
+  starts_at = "2028-08-24T00:00:00.000Z"
+  ends_at   = "2028-08-25T00:00:00.000Z"
 }
 
 # Complete Maintenance windows example
