c5cbe9fb42c22aac60c6dcf2a49edbbf55780965
Ignacio Anaya <nacho@checklyhq.com>
2022-06-30 09:00:28 -0300

    fix: set group private locations as optional

--- checkly/resource_check_group.go
+++ checkly/resource_check_group.go
@@ -53,7 +53,7 @@ func resourceCheckGroup() *schema.Resource {
 			},
 			"private_locations": {
 				Type:     schema.TypeSet,
-				Required: true,
+				Optional: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
