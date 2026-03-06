c3fa9b6dd0dfb5bf954b1ea2d0a39c6184f37857
Simo Kinnunen <simo@checklyhq.com>
2026-03-07 01:29:37 +0900

    feat: add environment variable support to playwright check suites

--- checkly/resource_playwright_check_suite_test.go
+++ checkly/resource_playwright_check_suite_test.go
@@ -1,1 +1,198 @@
 package checkly
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+const playwrightCheckSuiteBase = `
+	resource "checkly_playwright_code_bundle" "test" {
+		prebuilt_archive {
+			file = "../fixtures/playwright-project.tar.gz"
+		}
+	}
+`
+
+func TestAccPlaywrightCheckSuiteWithEnvironmentVariable(t *testing.T) {
+	accTestCase(t, []resource.TestStep{
+		{
+			Config: playwrightCheckSuiteBase + `
+				resource "checkly_playwright_check_suite" "test" {
+					name                      = "PW Check with env vars"
+					activated                 = true
+					frequency                 = 720
+					use_global_alert_settings = true
+					locations                 = ["us-east-1"]
+
+					bundle {
+						id       = checkly_playwright_code_bundle.test.id
+						metadata = checkly_playwright_code_bundle.test.metadata
+					}
+
+					runtime {
+						steps {
+							install {
+								command = "pnpm i"
+							}
+
+							test {
+								command = "pnpm playwright test"
+							}
+						}
+
+						playwright {
+							version = "1.56.1"
+							device {
+								type = "chromium"
+							}
+						}
+					}
+
+					environment_variable {
+						key    = "FOO"
+						value  = "bar"
+						locked = false
+					}
+
+					environment_variable {
+						key    = "SECRET"
+						value  = "s3cr3t"
+						locked = true
+					}
+				}
+			`,
+			Check: resource.ComposeTestCheckFunc(
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.#",
+					"2",
+				),
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.0.key",
+					"FOO",
+				),
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.0.value",
+					"bar",
+				),
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.0.locked",
+					"false",
+				),
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.1.key",
+					"SECRET",
+				),
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.1.value",
+					"s3cr3t",
+				),
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.1.locked",
+					"true",
+				),
+			),
+		},
+	})
+}
+
+func TestAccPlaywrightCheckSuiteEnvironmentVariableRemoval(t *testing.T) {
+	accTestCase(t, []resource.TestStep{
+		{
+			Config: playwrightCheckSuiteBase + `
+				resource "checkly_playwright_check_suite" "test" {
+					name                      = "PW Check env var removal"
+					activated                 = true
+					frequency                 = 720
+					use_global_alert_settings = true
+					locations                 = ["us-east-1"]
+
+					bundle {
+						id       = checkly_playwright_code_bundle.test.id
+						metadata = checkly_playwright_code_bundle.test.metadata
+					}
+
+					runtime {
+						steps {
+							install {
+								command = "pnpm i"
+							}
+
+							test {
+								command = "pnpm playwright test"
+							}
+						}
+
+						playwright {
+							version = "1.56.1"
+							device {
+								type = "chromium"
+							}
+						}
+					}
+
+					environment_variable {
+						key   = "FOO"
+						value = "bar"
+					}
+				}
+			`,
+			Check: resource.ComposeTestCheckFunc(
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.#",
+					"1",
+				),
+			),
+		},
+		{
+			Config: playwrightCheckSuiteBase + `
+				resource "checkly_playwright_check_suite" "test" {
+					name                      = "PW Check env var removal"
+					activated                 = true
+					frequency                 = 720
+					use_global_alert_settings = true
+					locations                 = ["us-east-1"]
+
+					bundle {
+						id       = checkly_playwright_code_bundle.test.id
+						metadata = checkly_playwright_code_bundle.test.metadata
+					}
+
+					runtime {
+						steps {
+							install {
+								command = "pnpm i"
+							}
+
+							test {
+								command = "pnpm playwright test"
+							}
+						}
+
+						playwright {
+							version = "1.56.1"
+							device {
+								type = "chromium"
+							}
+						}
+					}
+				}
+			`,
+			Check: resource.ComposeTestCheckFunc(
+				resource.TestCheckResourceAttr(
+					"checkly_playwright_check_suite.test",
+					"environment_variable.#",
+					"0",
+				),
+			),
+		},
+	})
+}
